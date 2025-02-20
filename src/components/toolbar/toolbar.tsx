3e7057419efe92b155bec7975966439ec3ed443d
DESKTOP-5HUBRHC\Leon-PC <leon.achteresch@gmail.com>
2025-02-20 18:00:29 +0100

    Füge CV-Bild hinzu und aktualisiere die Toolbar-Komponente

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,10 +1,11 @@
 import { cn } from "@/lib/utils";
 import Image, { type StaticImageData } from "next/image";
 import Link from "next/link";
-import RobotImage from "@/assets/Images/robot.png"
-import PhoneImage from "@/assets/Images/phone.webp"
-import GithubImage from "@/assets/Images/github.png"
-import LogoImage from "@/assets/Images/logo.png"
+import RobotImage from "@/assets/Images/robot.png";
+import PhoneImage from "@/assets/Images/phone.webp";
+import GithubImage from "@/assets/Images/github.png";
+import LogoImage from "@/assets/Images/logo.png";
+import CVImage from "@/assets/Images/CV.jpg";
 
 export function Dock() {
   return (
@@ -13,34 +14,14 @@ export function Dock() {
         <div className="relative ">
           <div className="pointer-events-none absolute right-0 bottom-0 left-0 z-10 h-[72px] max-w-full rounded-3xl border border-gray-200/60 bg-gray-200/60 shadow-sm dark:border-gray-600/60 dark:bg-gray-800/60" />
           <div className="flex items-end overflow-x-auto rounded-3xl pl-2">
-            <AppIcon
-              imgSrc={
-                PhoneImage
-              }
-            />
-            <AppIcon
-              imgSrc={
-                GithubImage
-              }
-              withoutBackground={true}
-            />
+            <AppIcon imgSrc={PhoneImage} />
+            <AppIcon imgSrc={GithubImage} withoutBackground={true} />
             <Link href="https://www.modul.day" target="_blank">
-              <AppIcon
-                imgSrc={
-                  RobotImage
-                }
-              />
+              <AppIcon imgSrc={RobotImage} />
             </Link>
+            <AppIcon imgSrc={CVImage} />
             <AppIcon
-              imgSrc={
-                RobotImage
-              }
-            />
-
-            <AppIcon
-              imgSrc={
-                LogoImage
-              }
+              imgSrc={LogoImage}
               // withoutBackground={true}
             />
           </div>
@@ -64,14 +45,14 @@ function AppIcon({
       <div
         className={cn(
           "pointer-events-none z-20 inline size-14 transform-gpu overflow-hidden rounded-2xl bg-white shadow-inner transition-all duration-200 group-hover:size-[4rem] group-hover:shadow-sm dark:bg-gray-800",
-          withoutBackground ? "p-2" : "",
+          withoutBackground ? "p-2" : ""
         )}
       >
         <Image
           alt="dock"
           className={cn(
             "size-full",
-            withoutBackground ? "object-contain" : "object-cover",
+            withoutBackground ? "object-contain" : "object-cover"
           )}
           height={256}
           src={imgSrc}
