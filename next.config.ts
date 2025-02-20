3bb34fe59acc7a9809c1bf88c070c8b750809b5b
DESKTOP-5HUBRHC\Leon-PC <leon.achteresch@gmail.com>
2025-02-20 17:11:01 +0100

    Füge ESLint-Konfiguration hinzu, um Fehler während des Produktionsbuilds zu ignorieren; aktualisiere Bildkonfiguration

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.iconscout.com',
-        port: '',
-        pathname: '/*',
-        search: '',
+        protocol: "https",
+        hostname: "cdn.iconscout.com",
+        port: "",
+        pathname: "/*",
+        search: "",
       },
     ],
   },
